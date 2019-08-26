9fb5e19a51ee5c9ca2024afdb1f84716a8afb6c2
Michael Fellinger <github@manveru.dev>
2019-08-27 01:23:37 +0200

    fix euphenix version

--- default.nix
+++ default.nix
@@ -1,4 +1,7 @@
-let euphenix = import ~/github/manveru/euphenix { };
+let euphenix = import (fetchTarball {
+  url = https://github.com/manveru/euphenix/archive/b29b1eb4ba3debc9d9db0354ba60d500b7bfecb7.tar.gz;
+  sha256 = "0pyg8wfxcr7y76zic0prqbzii0mwmls7y4bybzj9ir0vb8263155";
+}) { };
 in euphenix.build ./. {
   name = "david-anlagenbetreuung";
   favicon = ./favicon.svg;

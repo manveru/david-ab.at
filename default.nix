a002ad51e7032231c2e116de2957dad213cb4188
Michael Fellinger <github@manveru.dev>
2019-08-27 03:43:08 +0200

    bump euphenix

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
 let
   euphenix = import (fetchTarball {
     url =
-      "https://github.com/manveru/euphenix/archive/413caa1e48893eaeffeb081f2aac9123f0306147.tar.gz";
-    sha256 = "1y2nqnmdf3yhhjdcnxwkxjblys0y1y0bncis5p0pl166fqyz7kw1";
+      "https://github.com/manveru/euphenix/archive/7d4392f82adada0624f7d4745c375855b460a801.tar.gz";
+    sha256 = "0zlr3n4p2j046mhxyc0lh878hj7wyz6kyhb01cbb151n8l0jpgq2";
   }) { };
 in {
   euphenix = euphenix.euphenix;
